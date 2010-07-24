5635a9d526e4abd36a37cbc74ee9f2fe4f5e1e00
zrbecker <zrbecker@gmail.com>
2010-07-24 01:06:45 -0700

    Added header comments; added return 0; to main function

--- files/main.cpp
+++ files/main.cpp
@@ -1,10 +1,20 @@
+//
+// Project: Mimic
+// Description: Prints the command and its
+//   arguments to the stdout.
+// Author: Zachary Becker
+// Date: 2010-06-24
+//
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+	// Loops through command name and 
+	// its arguements
 	for (int i = 0; i < argc; i++) {
 		cout << argv[i] << " ";
 	}
 	cout << endl;
+	return 0;
 }
